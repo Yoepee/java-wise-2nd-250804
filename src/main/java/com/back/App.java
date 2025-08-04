f2351f2c79426c189a04299676a1ab539902651b
Yoepee <kdy2975@naver.com>
2025-08-04 14:34:32 +0900

    출력을 모니터가 아닌 별도의 문자열로 받을 수 있는지 확인

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -2,6 +2,8 @@ package com.back;
 
 import com.back.domain.system.controller.SystemController;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class App {
@@ -14,7 +16,13 @@ public class App {
     }
 
     public void run() {
+        PrintStream ORIGINAL_OUT = System.out;
         systemController.start();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream, true);
+        System.setOut(printStream);
+
+        System.out.println("반갑습니다.");
         String input = """
                 등록
                 안녕!!
@@ -25,11 +33,15 @@ public class App {
         String content = scanner.nextLine();
         System.out.println("명령어: " + cmd);
         System.out.println("내용: " + content);
-//        while(true){
-//            String cmd = sc.nextLine();
-//            System.out.println("명령어: " + cmd);
-//            if(cmd.equals("종료")) break;
-//        }
+        String str1 = byteArrayOutputStream.toString();
+
+        System.out.println("좋아요. 하하");
+        String str2 = byteArrayOutputStream.toString();
+        System.setOut(ORIGINAL_OUT);
+        printStream.close();
+
+        System.out.println("str1 : " + str1);
+        System.out.println("str2 : " + str2);
 
         systemController.stop();
     }
